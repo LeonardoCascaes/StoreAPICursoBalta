d7e577f15394498fe4835bd341eb91575d7518b6
Leonardo Cascaes <leonardo.cascaes@hotmail.com>
2022-05-18 21:08:59 -0300

    Efetuado a organização do projeto e criado entidades

---
+++ Store/Store.Domain/StoreContext/Entities/Product.cs
@@ -0,0 +1,25 @@
+﻿namespace Store.Domain.StoreContext.Entities
+{
+    public class Product
+    {
+        public Product(string tittle, string description, string image, decimal price, decimal quantityOnHand)
+        {
+            Tittle = tittle;
+            Description = description;
+            Image = image;
+            Price = price;
+            QuantityOnHand = quantityOnHand;
+        }
+
+        public string Tittle { get; private set; }
+        public string Description { get; private set; }
+        public string Image { get; private set; }
+        public decimal Price { get; private set; }
+        public decimal QuantityOnHand { get; private set; }
+
+        public override string ToString()
+        {
+            return Tittle;
+        }
+    }
+}
