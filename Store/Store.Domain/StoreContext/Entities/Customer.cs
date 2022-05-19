eceebd063d126440869950c804641dffd754c670
Leonardo Cascaes <leonardo.cascaes@hotmail.com>
2022-05-18 21:23:15 -0300

    Criado ValueObjects para evitar repetição de codigo no futuro

--- Store/Store.Domain/StoreContext/Entities/Customer.cs
+++ Store/Store.Domain/StoreContext/Entities/Customer.cs
@@ -1,27 +1,28 @@
-﻿namespace Store.Domain.StoreContext.Entities
+﻿using Store.Domain.StoreContext.ValueObjects;
+using System.Collections.Generic;
+
+namespace Store.Domain.StoreContext.Entities
 {
     public class Customer
     {
-        public Customer(string firstName, string lastName, string document, string email, string phone, string address)
+        public Customer(Name fullName, Document document, Email email, string phone)
         {
-            FirstName = firstName;
-            LastName = lastName;
+            FullName = fullName;
             Document = document;
             Email = email;
             Phone = phone;
-            Address = address;
+            Addresses = new List<Address>();
         }
 
-        public string FirstName { get; private set; }
-        public string LastName { get; private set; }
-        public string Document { get; private set; }
-        public string Email { get; private set; }
+        public Name FullName { get; private set; }
+        public Document Document { get; private set; }
+        public Email Email { get; private set; }
         public string Phone { get; private set; }
-        public string Address { get; private set; }
+        public IReadOnlyCollection<Address> Addresses { get; private set; }
 
         public override string ToString()
         {
-            return $"{FirstName} {LastName}";
+            return FullName.ToString();
         }
     }
 }
---
+++ Store/Store.Domain/StoreContext/ValueObjects/Document.cs
@@ -0,0 +1,17 @@
+﻿namespace Store.Domain.StoreContext.ValueObjects
+{
+    public class Document
+    {
+        public string Number { get; private set; }
+
+        public Document(string number)
+        {
+            Number = number;
+        }
+
+        public override string ToString()
+        {
+            return Number;
+        }
+    }
+}
