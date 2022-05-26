f80b64e1c90eee8ed0caec084b79ad5fb23196ff
Leonardo Cascaes <leonardo.cascaes@hotmail.com>
2022-05-26 20:32:24 -0300

    Criado testes de Document, Name, Order

---
+++ Store/Store.Tests/Domain/ValueObjects/NameTests.cs
@@ -0,0 +1,16 @@
+﻿using Store.Domain.StoreContext.ValueObjects;
+using Xunit;
+
+namespace Store.Tests.Domain.ValueObjects
+{
+    public class NameTests
+    {
+        [Fact]
+        public void DeveRetornarNotificacaoQuandoNomeNaoForValido()
+        {
+            var name = new Name("", "Cascaes");
+            Assert.False(name.IsValid);
+            Assert.Equal(1, name.Notifications.Count);
+        }
+    }
+}
